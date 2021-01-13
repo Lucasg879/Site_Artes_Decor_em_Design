c21540abcc5dd90a62fe08dac97b491a175e9846
lucasgago <lucasga12321@gmail.com>
2021-01-13 12:22:33 -0300

    Primeiro commite do código

---
+++ bec-end.js
@@ -0,0 +1,55 @@
+var imags=[];
+var slider;
+var imgAtual;
+var maxImg;
+var tmp;
+
+function preCarregameto(){
+    var s=1;
+    for(var i=0;i<3;i++){
+        imags[i]=new Image();
+        imags[i].src="s"+s+".jpg";
+        s++;
+    }
+}
+function carregarImg(img){
+    slider.style.backgroundImage="url('"+imags[img].src+"')"
+}
+function inicia(){
+    preCarregameto();
+    imgAtual=0;
+    maxImg=imags.length-1;
+    slider=document.getElementById("dvslider");
+    carregarImg(imgAtual); 
+    tmp=setInterval(troca,3000);
+}
+function troca(){
+    imgAtual++;
+    if(imgAtual>maxImg){
+        imgAtual=0;
+    }
+    carregarImg(imgAtual);
+}
+function scroll(){
+    let btn= document.getElementById("btnTop")
+    if(document.documentElement.scrollTop > 50){
+        btn.style.display="block";
+    }
+    else{
+        btn.style.display="none";
+    }
+}
+function backTop(){
+    document.documentElement.scrollTop = 0;
+}
+window.onscroll=function(){
+    scroll();
+}
+
+window.addEventListener("load",inicia);
+
+if (window.onload) {
+    document.getElementById("teste").innerText="A pagina foi carregada";
+}else{
+    document.getElementById("teste").innerText="A pagina não foi carregada ";
+}
